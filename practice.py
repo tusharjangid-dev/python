db71180a196bc19e25ad761e8a65b02bda33cd99
tusharjangid-dev <tusharjangid@w3erra.com>
2026-07-10 18:43:45 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,23 @@
+while True:
+    print("Hello")
+
+    choice = input("continue y/n:")
+
+    if choice == "n":
+        break
+
+
+# negetive numbers
+
+i = -10 
+while i <= -1:
+    print(i)
+    i += 1
+
+
+
+# negetive numbers in for loop 
+
+for i in range(-5,0,1):
+    print(i)
+
